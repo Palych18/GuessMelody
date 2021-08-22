082168b0b8cf93aedc9b250db033abbc363e3ca3
Palych18 <pavel_egoroff@mail.ru>
2021-08-22 20:02:49 +0300

    Добавьте файлы проекта.

---
+++ GuessMelody/fSettings.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace GuessMelody
+{
+    partial class fSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fSettings));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.cbAllDirectory = new System.Windows.Forms.CheckBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCansel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbGameDuration = new System.Windows.Forms.ComboBox();
+            this.cbMusicDuration = new System.Windows.Forms.ComboBox();
+            this.cbRandomStart = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(505, 459);
+            this.listBox1.TabIndex = 0;
+            // 
+            // cbAllDirectory
+            // 
+            this.cbAllDirectory.AutoSize = true;
+            this.cbAllDirectory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbAllDirectory.Location = new System.Drawing.Point(535, 360);
+            this.cbAllDirectory.Name = "cbAllDirectory";
+            this.cbAllDirectory.Size = new System.Drawing.Size(164, 24);
+            this.cbAllDirectory.TabIndex = 1;
+            this.cbAllDirectory.Text = "Вложенные папки";
+            this.cbAllDirectory.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(535, 275);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(134, 39);
+            this.btnSelectFolder.TabIndex = 2;
+            this.btnSelectFolder.Text = "Выбрать папку";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClearList.Location = new System.Drawing.Point(712, 275);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(134, 39);
+            this.btnClearList.TabIndex = 3;
+            this.btnClearList.Text = "Очистить";
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearList_Click);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(682, 426);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCansel
+            // 
+            this.btnCansel.Location = new System.Drawing.Point(786, 426);
+            this.btnCansel.Name = "btnCansel";
+            this.btnCansel.Size = new System.Drawing.Size(75, 23);
+            this.btnCansel.TabIndex = 5;
+            this.btnCansel.Text = "Отмена";
+            this.btnCansel.UseVisualStyleBackColor = true;
+            this.btnCansel.Click += new System.EventHandler(this.btnCansel_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbRandomStart);
+            this.groupBox1.Controls.Add(this.cbMusicDuration);
+            this.groupBox1.Controls.Add(this.cbGameDuration);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(511, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 231);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки игры";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Продолжительность игры";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Время на ответ";
+            // 
+            // cbGameDuration
+            // 
+            this.cbGameDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbGameDuration.FormattingEnabled = true;
+            this.cbGameDuration.Items.AddRange(new object[] {
+            "30",
+            "45",
+            "60",
+            "75",
+            "90",
+            "105",
+            "120"});
+            this.cbGameDuration.Location = new System.Drawing.Point(241, 50);
+            this.cbGameDuration.Name = "cbGameDuration";
+            this.cbGameDuration.Size = new System.Drawing.Size(68, 28);
+            this.cbGameDuration.TabIndex = 2;
+            this.cbGameDuration.Text = "60";
+            // 
+            // cbMusicDuration
+            // 
+            this.cbMusicDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbMusicDuration.FormattingEnabled = true;
+            this.cbMusicDuration.Items.AddRange(new object[] {
+            "5",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30"});
+            this.cbMusicDuration.Location = new System.Drawing.Point(241, 99);
+            this.cbMusicDuration.Name = "cbMusicDuration";
+            this.cbMusicDuration.Size = new System.Drawing.Size(68, 28);
+            this.cbMusicDuration.TabIndex = 3;
+            this.cbMusicDuration.Text = "10";
+            // 
+            // cbRandomStart
+            // 
+            this.cbRandomStart.AutoSize = true;
+            this.cbRandomStart.Checked = true;
+            this.cbRandomStart.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbRandomStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbRandomStart.Location = new System.Drawing.Point(10, 168);
+            this.cbRandomStart.Name = "cbRandomStart";
+            this.cbRandomStart.Size = new System.Drawing.Size(188, 24);
+            this.cbRandomStart.TabIndex = 4;
+            this.cbRandomStart.Text = "Со случайного места";
+            this.cbRandomStart.UseVisualStyleBackColor = true;
+            // 
+            // fSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCansel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnClearList);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.cbAllDirectory);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fSettings";
+            this.Text = "Настройки";
+            this.Load += new System.EventHandler(this.fSettings_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox cbAllDirectory;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnClearList;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCansel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbMusicDuration;
+        private System.Windows.Forms.ComboBox cbGameDuration;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbRandomStart;
+    }
+}
---
+++ GuessMelody/fSettings.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GuessMelody
+{
+    public partial class fSettings : Form
+    {
+        public fSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Victorina.allDirectories = cbAllDirectory.Checked;
+            Victorina.gameDuration = Convert.ToInt32(cbGameDuration.Text);
+            Victorina.musicDuration = Convert.ToInt32(cbMusicDuration.Text);
+            Victorina.randomStart = cbRandomStart.Checked;
+            Victorina.WriteParam();
+            this.Hide();
+        }
+
+        void Set()
+        {
+            cbAllDirectory.Checked = Victorina.allDirectories;
+            cbRandomStart.Checked = Victorina.randomStart;
+            cbGameDuration.Text = Victorina.gameDuration.ToString();
+            cbMusicDuration.Text = Victorina.musicDuration.ToString();
+        }
+
+        private void btnCansel_Click(object sender, EventArgs e)
+        {
+            Set();
+            this.Hide();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                string[] musicList = Directory.GetFiles(fbd.SelectedPath, "*.mp3", cbAllDirectory.Checked? SearchOption.AllDirectories: SearchOption.TopDirectoryOnly);
+                Victorina.lastFolder = fbd.SelectedPath;
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(musicList);
+                Victorina.list.Clear();
+                Victorina.list.AddRange(musicList);
+            }
+        }
+
+        private void btnClearList_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void fSettings_Load(object sender, EventArgs e)
+        {
+            Set();
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(Victorina.list.ToArray());
+        }
+    }
+}
